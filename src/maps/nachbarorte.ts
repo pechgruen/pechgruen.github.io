2ebeb8c1c504e365e229054f5e4dfc542a03766e
Udo Dengler <udo_dengler@yahoo.com>
2026-01-15 21:02:29 -0500

    um-pechgruen.astro umgemodelt und deepp zoom karte angepasst

---
+++ src/maps/nachbarorte.ts
@@ -0,0 +1,16 @@
+export const ortschaftenUmPechgruen = {
+  id: "dz-nachbarorte",
+
+  tileSource: "/tiles/nachbarorte/map.dzi",
+
+  startView: {
+    type: "centerZoom" as const,
+    x: 2765,
+    y: 3434,
+    zoomFactor: 2.3,
+  },
+
+  markersDefaultVisible: false,
+  showMarkersToggle: true,
+  authorMode: true,
+};
